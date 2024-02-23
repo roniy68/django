c44d6f08a5eb09042d4046b1bd9dc1fa3585885a
roniy68 <ahmed.11900449@lpu.in>
2024-02-24 01:38:26 +0600

    🔥 Add urlpatterns for development
    Signed-off-by: roniy68 <ahmed.11900449@lpu.in>

--- app/app/urls.py
+++ app/app/urls.py
@@ -20,3 +20,18 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )

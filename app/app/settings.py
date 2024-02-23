401faad546782f29398783cb73fa10e3a14c1d42
roniy68 <ahmed.11900449@lpu.in>
2024-02-24 00:49:11 +0600

    🔥 Add settings for debug and allowed hosts
    Signed-off-by: roniy68 <ahmed.11900449@lpu.in>

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8j&vjcf@msr7(^$_$vlotkq4&^s@0mtqhrf*kx(1^$kmw$(n1h'
+# SECRET_KEY = 'django-insecure-8j&vjcf@msr7(^$_$vlotkq4&^s@0mtqhrf*kx(1^$kmw$(n1h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
